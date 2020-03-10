75a67bd37d082005cd0ac668c810192ed183b36e
Denis Dunaievskiy <denver9523@gmail.com>
2020-03-10 18:34:20 +0200

    Updates

---
+++ precache-manifest.287c4911f9559d02dee5593d8cefb3d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b9adf42d0f96aeb0b10e10290b2bc5",
+    "url": "/kopilka-react/index.html"
+  },
+  {
+    "revision": "b02f310ac50849d1b4e7",
+    "url": "/kopilka-react/static/css/main.97b697b7.chunk.css"
+  },
+  {
+    "revision": "2aba6f0d310dfb332b91",
+    "url": "/kopilka-react/static/js/2.c6fe8893.chunk.js"
+  },
+  {
+    "revision": "b02f310ac50849d1b4e7",
+    "url": "/kopilka-react/static/js/main.36d0b9d5.chunk.js"
+  },
+  {
+    "revision": "bdd3bcf3760a8f407031",
+    "url": "/kopilka-react/static/js/runtime-main.749b4b8e.js"
+  },
+  {
+    "revision": "419b45388b591cb76de2a07891df90f2",
+    "url": "/kopilka-react/static/media/pageNotFound.419b4538.jpg"
+  },
+  {
+    "revision": "fae11f9796dfaf0671b623b2017b36c6",
+    "url": "/kopilka-react/static/media/preloader.fae11f97.svg"
+  }
+]);
